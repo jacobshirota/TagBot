f225281f6d13929ab9c7d525447d1646fd3e15c4
jacobshirota <114620967+jacobshirota@users.noreply.github.com>
2023-11-08 14:51:26 -0800

    Update config.py
    Made it less stupid by using python's idea of a switch/case statement, added initial_config to config.ini for use with logger

--- config.py
+++ config.py
@@ -7,59 +7,34 @@ debug_settings = configP['debug_settings']
 game_settings = configP['game_settings']
 pause_settings = configP['pause_settings']
 
+
 def update():
     with open('config.ini', 'w') as file:
         configP.write(file)
 
 
-def get_debug_mode():
-    return debug_settings.getboolean('debug_mode')
-
-def get_active():
-    return game_settings.getboolean('active')
-
-def get_start_time():
-    return game_settings.getint('start_time')
-
-def get_end_time():
-    return game_settings.getint('end_time')
-
-def get_cooldown():
-    return game_settings.getint('cooldown')
-
-def get_paused():
-    return pause_settings.getboolean('paused')
-
-def get_resume_time():
-    return pause_settings.getint('resume_time')
+def cget(arg):
+    match arg:
+        case 'debug_mode' | 'initial_config':
+            return debug_settings.getboolean(arg)
+        case 'active':
+            return game_settings.getboolean(arg)
+        case 'start_time' | 'end_time' | 'cooldown':
+            return game_settings.getint(arg)
+        case 'paused':
+            return pause_settings.getboolean(arg)
+        case 'resume_time':
+            return pause_settings.getint(arg)
 
 
-def set_debug_mode(val):
-    debug_settings['debug_mode'] = str(val)
-    update()
-
-def set_active(val):
-    game_settings['active'] = str(val)
-    update()
-
-def set_start_time(val):
-    game_settings['start_time'] = str(val)
-    update()
-
-def set_end_time(val):
-    game_settings['end_time'] = str(val)
-    update()
-
-def set_cooldown(val):
-    game_settings['cooldown'] = str(val)
-    update()
-
-def set_paused(val):
-    pause_settings['paused'] = str(val)
-    update()
-
-def set_resume_time(val):
-    pause_settings['resume_time'] = str(val)
+def cset(*args):
+    match args[0]:
+        case 'debug_mode' | 'initial_config':
+            debug_settings[args[0]] = str(args[1])
+        case 'active' | 'start_time' | 'end_time' | 'cooldown':
+            game_settings[args[0]] = str(args[1])
+        case 'paused' | 'resume_time':
+            pause_settings[args[0]] = str(args[1])
     update()
 
 

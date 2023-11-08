db879db43b99f26d859f6573d54cdb54b35bfb8a
jacobshirota <114620967+jacobshirota@users.noreply.github.com>
2023-11-08 15:30:34 -0800

    Update init.sql
    Because I'm a lil stupid and wrote for MySQL instead of SQLite

--- init.sql
+++ init.sql
@@ -1,23 +1,23 @@
 CREATE TABLE users (
-    UserID INT NOT NULL,
-    Mention VARCHAR(38),
-    Playing ENUM('True', 'False'),
-    It ENUM('True', 'False'),
+    UserID INTEGER NOT NULL,
+    Mention TEXT,
+    Playing TEXT CHECK (Playing IN ('True', 'False')),
+    It TEXT CHECK (It IN ('True', 'False')),
     CONSTRAINT users_pk PRIMARY KEY (UserID)
 );
 
 CREATE TABLE game_logs (
     Timestamp INT NOT NULL,
-    GameID INT,
-    Event ENUM('START', 'END', 'PAUSE', 'RESUME', 'TAG'),
-    UserID INT,
+    GameID INTEGER,
+    Event TEXT CHECK (Event IN ('START', 'END', 'PAUSE', 'RESUME', 'TAG')),
+    UserID INTEGER,
     CONSTRAINT game_logs_pk PRIMARY KEY (Timestamp),
     CONSTRAINT users_logs_fk FOREIGN KEY (UserID) REFERENCES users (UserID)
 );
 
 CREATE TABLE leaderboard (
-    UserID INT NOT NULL,
-    TotalTime INT,
+    UserID INTEGER NOT NULL,
+    TotalTime INTEGER,
     CONSTRAINT leaderboard_pk PRIMARY KEY (UserID),
     CONSTRAINT user_leaderboard_fk FOREIGN KEY (UserID) REFERENCES users (UserID)
 );

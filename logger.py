3dee12a52d02f8e09aa1def358ca49a680ce430d
jacobshirota <114620967+jacobshirota@users.noreply.github.com>
2023-11-11 21:33:57 -0800

    More work on logger.py
    fleshed out the functions needed to work on the other tables. untested.

--- logger.py
+++ logger.py
@@ -10,17 +10,52 @@ if config.cget('initial_config'):
     config.cset('initial_config', 'False')
 
 
+# game_logs functions
+
 def log(*args):
-    query = "INSERT INTO game_logs VALUES (" + config.cget('start_time')
+    query = "INSERT INTO game_logs VALUES (" + str(int(time.time())) + ", " + config.cget('start_time')
     if args[0] in ['START', 'END', 'PAUSE', 'RESUME']:
-        # add case for immediate start
-        query += ", '" + args[0] + "', NULL, "
+        query += ", '" + args[0] + "', NULL);"
     elif args[0] == 'TAG':
-        query += ", 'TAG', " + args[1] + ", "
-    query += str(int(time.time())) + ");"
+        query += ", 'TAG', " + args[1] + ");"
     db.execute(query)
+    return True
 
 
-# ADD add_user(user)
+# user functions
+def add_user(user):
+    check = db.execute("SELECT * FROM users WHERE UserID=" + str(user.id) + ");")
+    if check.fetchone() is not None:
+        return False
+    query = "INSERT INTO users VALUES (" +  str(user.id) + ", " + user.mention + ", " + "False, False);"
+    db.execute(query)
+    query = "INSERT INTO leaderboard VALUES(" + str(user.id) + ", 0);"
+    db.execute(query)
+    return True
 
+# used to set Playing, It
+def user_set(user, row):
+    check = db.execute("SELECT" + row + " FROM users WHERE UserID=" + str(user.id) + ");")
+    if check.fetchone() is None:
+        add_user(user)
+    query = "UPDATE users SET " + row + "="
+    query += 'True' if check.fetchone()[0] == 'False' else 'False'
+    query += "WHERE UserID=" + str(user.id) + ");"
+    db.execute(query)
+    return True if check.fetchone()[0] == 'False' else False
+
+def user_check(user, row):
+    check = db.execute("SELECT" + row + " FROM users WHERE UserID=" + str(user.id) + ");")
+    if check.fetchone() is None:
+        return False
+    return True if check.fetchone()[0] == 'True' else False
+
+def user_set_all(row, val):
+    db.execute("UPDATE users SET" + row + "=" + val)
+
+
+# leaderboard functions
+def get_leaderboard():
+    check = db.execute("SELECT Mention, TotalTime FROM user, leaderboard);")
+    return check.fetchall()
 

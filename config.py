6830b0bef32f0277e793905785509406f63a18ea
jacobshirota <114620967+jacobshirota@users.noreply.github.com>
2023-11-20 16:19:35 -0800

    Added guild_settings to assist in role management. Will require owner to manually input guild and role IDs

--- config.py
+++ config.py
@@ -6,6 +6,7 @@ configP.read('config.ini')
 debug_settings = configP['debug_settings']
 game_settings = configP['game_settings']
 pause_settings = configP['pause_settings']
+guild_settings = configP['guild_settings']
 
 
 def update():
@@ -24,10 +25,15 @@ def cget(arg):
         return pause_settings.getboolean(arg)
     elif arg == 'resume_time':
         return pause_settings.getint(arg)
+    elif arg in ['guild_id', 'playing_role', 'it_role', 'not_it_role']:
+        if guild_settings[arg] == 'NULL':
+            return None
+        else:
+            return guild_settings.getint(arg)
 
 
 def cset(*args):
-    if args[0] in  ['debug_mode', 'initial_config']:
+    if args[0] in ['debug_mode', 'initial_config']:
         debug_settings[args[0]] = str(args[1])
     elif args[0] in ['active', 'start_time', 'end_time', 'cooldown']:
         game_settings[args[0]] = str(args[1])
